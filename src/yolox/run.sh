2e66ed653d6deee5c43b7b51fd5d0cdd2958bf97
lorenzo <baiyingpoi7z@gmail.com>
2023-05-26 03:16:30 +0000

    update requriement.txt & yolov5 infer by onnxruntime

--- src/yolox/run.sh
+++ src/yolox/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-pip install mmcv==2.0.0
-pip install terminaltables
-pip install pycocotools
-pip install shapely
-
 cd mmdetection
 wget -P ../res/ https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_tiny_8x8_300e_coco/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth
 python export.py
