80afed92ec8e2a5953ddb639f0b99ab0ea0abc5c
lorenzo <baiyingpoi7z@gmail.com>
2023-05-26 07:22:38 +0000

    update yolov5-openvino

--- src/yolov5/run.sh
+++ src/yolov5/run.sh
@@ -2,6 +2,7 @@
 
 bash export-yolov5-6.0.sh
 bash detect-for-yolov5-6.0.sh
+python inference-with-onnxruntime.py
 
 mkdir build
 cd build
@@ -9,3 +10,9 @@ cmake ..
 make -j32
 cd ../res
 ./yolov5
+
+./yolov5_ort
+
+#* to solve the problem of "libtbb.so.2: cannot open shared object file: No such file or directory"
+export LD_LIBRARY_PATH=../../../include/cpp-packages/openvino_2022.1.0.643/runtime/3rdparty/tbb/lib/:$LD_LIBRARY_PATH
+./yolov5_openvino
